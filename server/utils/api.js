31fc3ecf593dd985057c4d1d2aa156effa8c8455
jackrock1723-svg <jackrock1723@gmail.com>
2025-09-28 14:56:06 +0530

    fix: updated both frontend and  backend code

--- server/utils/api.js
+++ server/utils/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 import { getSellerToken, clearSellerToken } from "./auth";
 
-const API_BASE = process.env.REACT_APP_BACKEND_URL || "https://api.shienstore.com/";
+const API_BASE = process.env.REACT_APP_BACKEND_URL;
 
 const api = axios.create({
   baseURL: API_BASE,
