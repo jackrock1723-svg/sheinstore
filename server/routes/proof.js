759d9d7030cd197dce0c8e78159b2aa3dfca4a2c
jackrock1723-svg <jackrock1723@gmail.com>
2025-09-28 17:44:04 +0530

    fix: updated both frontend and  backend code

--- server/routes/proof.js
+++ server/routes/proof.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const authMiddleware = require("../middleware/auth");
+const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
