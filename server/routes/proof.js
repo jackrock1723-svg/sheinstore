26b868172779ee26f516eb47a49ff0fb20774bf5
jackrock1723-svg <jackrock1723@gmail.com>
2025-09-28 17:21:42 +0530

    fix: updated both frontend and  backend code

--- server/routes/proof.js
+++ server/routes/proof.js
@@ -3,7 +3,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const router = express.Router();
-const authMiddleware = require("../middleware/auth");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.get("/:filename", authMiddleware(["admin", "seller"]), (req, res) => {
   // Ensure correct folder (adjust if needed)
